814612e6a4b30396c740b5893b254a23b781c824
mayuzee <emilhansbert@gmail.com>
2024-01-07 18:56:49 +0100

    Seite Team

--- frontend/src/pages/Blog/Blog.tsx
+++ frontend/src/pages/Blog/Blog.tsx
@@ -8,16 +8,16 @@ export const Blog:FC = () => {
 
     return (
         <>
-            <h3>Blogtitel</h3>
-            <span>20.11.2023 von Maris</span>
-            <img src="" alt="Titelbild" />
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia id doloremque deserunt perferendis praesentium, facilis reiciendis excepturi optio neque. Deserunt non dolorem esse, ipsa optio voluptate ducimus aliquam dolores suscipit.</p>
-            <div className="images">
-                <img src="" alt="Bild1" />
-                <img src="" alt="Bild2" />
-            </div>
-            <h1>Weitere Posts</h1>
-            <BlogOverview blogcount={3} />
+                <h3>Blogtitel</h3>
+                <span>20.11.2023 von Maris</span>
+                <img src="" alt="Titelbild" />
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia id doloremque deserunt perferendis praesentium, facilis reiciendis excepturi optio neque. Deserunt non dolorem esse, ipsa optio voluptate ducimus aliquam dolores suscipit.</p>
+                <div className="images">
+                    <img src="" alt="Bild1" />
+                    <img src="" alt="Bild2" />
+                </div>
+                <h1>Weitere Posts</h1>
+                <BlogOverview blogcount={3} />
         </>
     )
 }
