0a9b769503f23529df5ba91d6c92627a02059f41
flerovium11 <enniobinder@gmail.com>
2024-01-21 20:01:19 +0100

    fix login error

--- frontend/src/pages/Login/Login.tsx
+++ frontend/src/pages/Login/Login.tsx
@@ -1,19 +1,57 @@
-import {FC} from 'react'
-import { useTranslation } from 'react-i18next'
-import './Login.scss'
-import 'bootstrap/scss/bootstrap.scss'
+import React, { FC } from 'react';
+import { useTranslation } from 'react-i18next';
+import 'bootstrap/dist/css/bootstrap.min.css'; // Import Bootstrap CSS
+import './Login.scss';
 
-export const Login:FC = () => {
-    const {t} = useTranslation()
+export const Login: FC = () => {
+  const { t } = useTranslation();
 
-    return (
-        <>
-            <h3>Admin Login</h3>
-            <form>
-                <input type='text' id='username' placeholder={t()}/>
-                <input type='text' id='password' />
-                <button type='submit'>{t('login')}</button>
-            </form>
-        </>
-    )
-}
+  return (
+    <>
+      <div className="container py-5 mt-5">
+        <h3>Admin Login</h3>
+        <form
+          onSubmit={(e) => {
+            e.preventDefault();
+          }}
+        >
+          <div className="form-group">
+            <label htmlFor="exampleInputEmail1">Email address</label>
+            <input
+              type="email"
+              className="form-control"
+              id="exampleInputEmail1"
+              aria-describedby="emailHelp"
+              placeholder="Enter email"
+            />
+            <small id="emailHelp" className="form-text text-muted">
+              We'll never share your email with anyone else.
+            </small>
+          </div>
+          <div className="form-group">
+            <label htmlFor="exampleInputPassword1">Password</label>
+            <input
+              type="password"
+              className="form-control"
+              id="exampleInputPassword1"
+              placeholder="Password"
+            />
+          </div>
+          <div className="form-check">
+            <input
+              type="checkbox"
+              className="form-check-input"
+              id="exampleCheck1"
+            />
+            <label className="form-check-label" htmlFor="exampleCheck1">
+              Remember me
+            </label>
+          </div>
+          <button type="submit" className="btn btn-primary">
+            Submit
+          </button>
+        </form>
+      </div>
+    </>
+  );
+};
